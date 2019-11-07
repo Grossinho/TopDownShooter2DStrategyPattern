6a28408b9ca0354fefac18aeeb121b3e32318c6a
Mobbit <mobbit031@gmail.com>
2019-11-07 17:24:18 -0300

    no message

---
+++ TopDownShooter2DStrategyPattern/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    float movement;
+    Rigidbody2D rb2d;
+    Vector3 mousePosition;
+    Quaternion rotation;
+
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rotation = 
+            Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = rotation;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        movement = Input.GetAxis("Vertical");
+        rb2d.AddForce(gameObject.transform.up * movement * speed);
+    }
+}
---
+++ TopDownShooter2DStrategyPattern/Assets/Scripts/TiroSimples.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroSimples : Arma, IArma
+{
+
+    private float speed;
+    private Transform firePoint;
+    
+    void Start()
+    {
+        firePoint = GetComponentInChildren<Transform>().GetChild(0);
+    }
+   
+    public void Atirar()
+    {
+        GameObject bullet = Instantiate(Resources.Load("Bullet", typeof(GameObject)))
+            as GameObject;
+        bullet.transform.position = firePoint.position;
+        bullet.transform.rotation = firePoint.rotation;
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+            
+    }
+}
