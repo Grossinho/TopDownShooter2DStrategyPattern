07ef9df60cb4dd941adbdb0ca142bca26af5bc93
Mobbit <mobbit031@gmail.com>
2019-11-11 17:26:08 -0300

    no message

--- TopDownShooter2DStrategyPattern/Assets/Scripts/PlayerController.cs
+++ TopDownShooter2DStrategyPattern/Assets/Scripts/PlayerController.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] string nomearma;
 
     float movement;
     Rigidbody2D rb2d;
     Vector3 mousePosition;
     Quaternion rotation;
+    IArma arma;
 
     
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        configArma(nomearma);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+            arma.Atirar();
     }
 
     private void FixedUpdate()
@@ -34,5 +38,39 @@ public class PlayerController : MonoBehaviour
 
         movement = Input.GetAxis("Vertical");
         rb2d.AddForce(gameObject.transform.up * movement * speed);
+    }
+
+    public void configArma(string tag)
+    {
+        switch (tag)
+        {
+            case "Pickup":
+                RemoveArma();
+                this.arma = gameObject.AddComponent<TiroSimples>();
+                break;
+            case "PickupGrande":
+                RemoveArma();
+                this.arma = gameObject.AddComponent<TiroGrande>();
+                break;
+            case "UFO":
+                RemoveArma();
+                this.arma = gameObject.AddComponent<TiroRayCast>();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void RemoveArma()
+    {
+        // PARA EVITAR A CRIACAO DE MULTIPLOS COMPONENTES DO MESMO TIPO NO GAMEOBJECT
+        Component c = gameObject.GetComponent<IArma>() as Component;
+        if (c != null) Destroy(c);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Enemy"))
+            configArma(collision.tag);
     }
 }
--- TopDownShooter2DStrategyPattern/Assets/Scripts/TiroGrande.cs
+++ TopDownShooter2DStrategyPattern/Assets/Scripts/TiroGrande.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TiroSimples : Arma, IArma
+public class TiroGrande : Arma, IArma
 {
 
     private float speed;
     private Transform firePoint;
-    
+
     void Start()
     {
         firePoint = GetComponentInChildren<Transform>().GetChild(0);
-        speed = 10;
+        speed = 5;
     }
-   
+
     public void Atirar()
     {
-        GameObject bullet = Instantiate(Resources.Load("Bullet", typeof(GameObject)))
+        GameObject bullet = Instantiate(Resources.Load("BulletGrande", typeof(GameObject)))
             as GameObject;
         bullet.transform.position = firePoint.position;
         bullet.transform.rotation = firePoint.rotation;
         bullet.GetComponent<Rigidbody2D>().velocity = transform.up * speed;
-            
+
     }
 }
